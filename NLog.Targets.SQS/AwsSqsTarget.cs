fb3dc2c0779b6722528e08b9968aa68a27af8838
Eric Anastas <aireq303@gmail.com>
2016-11-09 13:01:47 -0800

    Added batch messages. Switched AwsSqsTarget to inherit from TargetWithLayout.

--- NLog.Targets.SQS/AwsSqsTarget.cs
+++ NLog.Targets.SQS/AwsSqsTarget.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,17 @@ using System.Threading.Tasks;
 namespace NLog.Targets.SQS
 {
     [Target("SQS")]
-    public class AwsSqsTarget : NLog.Targets.Target, IDisposable
+    public class AwsSqsTarget : NLog.Targets.TargetWithLayout, IDisposable
     {
+
+        public const int DEFAULT_BATCH_SIZE = 10;
+        public const int MIN_BATCH_SIZE = 1;
+        public const int MAX_BATCH_SIZE = 10;
 
         [NLog.Config.RequiredParameter]
         public string RegionEndPoint { get; set; }
+
+        public bool ThrowExceptions { get; set; }
 
         [NLog.Config.RequiredParameter]
         public string QueueUrl { get; set; }
@@ -21,8 +28,42 @@ namespace NLog.Targets.SQS
         [NLog.Config.RequiredParameter]
         public string AwsAccessKeyId { get; set; }
 
+
         [NLog.Config.RequiredParameter]
         public string AwsSecretAccessKey { get; set; }
+
+
+        public AwsSqsTarget()
+        {
+            BatchSize = DEFAULT_BATCH_SIZE;
+        }
+
+        private int _batchSize;
+        public int BatchSize
+        {
+            get
+            {
+                return _batchSize;
+            }
+            set
+            {
+
+                if (value < MIN_BATCH_SIZE | value > MAX_BATCH_SIZE)
+                {
+                    string m = "BatchSize can not be set to " + value.ToString() + ", it must be between " + MIN_BATCH_SIZE + " and " + MAX_BATCH_SIZE + ". Defaulting to " + DEFAULT_BATCH_SIZE;
+                    InternalLogger.Error(m);
+
+                    _batchSize = DEFAULT_BATCH_SIZE;
+
+                    if (ThrowExceptions) throw new ArgumentException(m);
+                }
+                else
+                {
+                    _batchSize = value;
+                }
+            }
+        }
+
 
 
         private Amazon.SQS.AmazonSQSClient _client;
@@ -32,53 +73,113 @@ namespace NLog.Targets.SQS
         {
             base.InitializeTarget();
             var region = Amazon.RegionEndpoint.GetBySystemName(RegionEndPoint);
-            _client = new Amazon.SQS.AmazonSQSClient(AwsAccessKeyId, AwsSecretAccessKey,region);
+            _client = new Amazon.SQS.AmazonSQSClient(AwsAccessKeyId, AwsSecretAccessKey, region);
         }
 
-        protected override void Write(LogEventInfo logEvent)
+
+        protected override void Write(AsyncLogEventInfo logEvent)
         {
+            Write(new[] { logEvent });
+        }
 
-            _client.SendMessage(QueueUrl, logEvent.Message);
+        protected override void Write(AsyncLogEventInfo[] logEvents)
+        {
+            try
+            {
+                List<AsyncLogEventInfo> batchList = new List<AsyncLogEventInfo>();
 
+                foreach (var logEvent in logEvents)
+                {
+                    batchList.Add(logEvent);
 
+                    if (batchList.Count == BatchSize)
+                    {
+                        SendBatch(batchList.Select(e => e.LogEvent));
+                        batchList.Clear();
+                    }
+                }
 
-            Amazon.SQS.Model.SendMessageRequest req = new Amazon.SQS.Model.SendMessageRequest(QueueUrl, "message body");
-
-
-            //logEvent.Exception;
-            //logEvent.FormatProvider;
-            //logEvent.FormattedMessage;
-            //logEvent.HasStackTrace;
-            //logEvent.Level;
-            //logEvent.LoggerName;
-            //logEvent.Message;
-            //logEvent.Parameters;
-            //logEvent.Properties;
-            //logEvent.SequenceID;
-            //logEvent.StackTrace;
-            //logEvent.TimeStamp;
-            //logEvent.UserStackFrame;
-            //logEvent.UserStackFrameNumber;
-
-
-
-
-
-            //req.MessageBody;
-            //req.QueueUrl;
-            //req.DelaySeconds;
+                if (batchList.Count > 0) SendBatch(batchList.Select(e => e.LogEvent));
+            }
+            catch (Exception exp)
+            {
+                InternalLogger.Error(exp, "Error while sending log messages to Amazon SQS: message=\"{0}\"", exp.Message);
+                if (ThrowExceptions) throw;
+            }
+        }
 
 
+        private void SendBatch(IEnumerable<LogEventInfo> logEvents)
+        {
+            //Checks target is initialized and not disposed
+            if (_client == null)
+            {
+                throw new ObjectDisposedException("AwsSqsTarget", "AmazonSQSClient for AwsSqsTarget is NULL. Target may not have been initialized or was disposed.");
+            }
+
+            //Create batch of messages
+            List<Amazon.SQS.Model.SendMessageBatchRequestEntry> sendMesssageReqs = new List<Amazon.SQS.Model.SendMessageBatchRequestEntry>();
+
+            int id = 0;
+
+            foreach (var logEvent in logEvents)
+            {
+                //Renders the message using the current layout
+                Amazon.SQS.Model.SendMessageBatchRequestEntry batchReqEntry = new Amazon.SQS.Model.SendMessageBatchRequestEntry();
 
 
-            //if (_messageDespatcher == null)
-            //    throw new ArgumentNullException(nameof(_messageDespatcher));
+                batchReqEntry.DelaySeconds = DelaySeconds;
+                batchReqEntry.MessageBody = this.Layout.Render(logEvent);
+                batchReqEntry.Id = id.ToString();
 
-            //if (logEvent?.LoggerName?.Equals("Amazon", StringComparison.InvariantCultureIgnoreCase) ?? false)   //prevent an infinite loop
-            //    return;
 
-            //var message = Layout.Render(logEvent);
-            //_messageDespatcher.DespatchAsync(GetTopicArn(), message).ConfigureAwait(false).GetAwaiter().GetResult();
+                //Write message attributes
+                batchReqEntry.MessageAttributes.Add("Logger", new Amazon.SQS.Model.MessageAttributeValue() { StringValue = logEvent.LoggerName, DataType = "String" });
+                batchReqEntry.MessageAttributes.Add("Level", new Amazon.SQS.Model.MessageAttributeValue() { StringValue = logEvent.Level.ToString(), DataType = "String" });
+                batchReqEntry.MessageAttributes.Add("SequenceID", new Amazon.SQS.Model.MessageAttributeValue() { StringValue = logEvent.SequenceID.ToString(), DataType = "Number" });
+
+
+                //Other LogEventInfo Properties ...
+
+                //logEvent.Parameters;
+                //logEvent.Properties;
+                //logEvent.Message;
+                //logEvent.FormattedMessage;
+                //logEvent.TimeStamp;
+                //logEvent.Exception;
+                //logEvent.FormatProvider;
+                //logEvent.HasStackTrace;
+                //logEvent.StackTrace;
+                //logEvent.UserStackFrame;
+                //logEvent.UserStackFrameNumber;
+
+
+
+                sendMesssageReqs.Add(batchReqEntry);
+
+                id++; //increment message id;
+            }
+
+
+            //Sends the batch of messages
+            Amazon.SQS.Model.SendMessageBatchRequest batchReq = new Amazon.SQS.Model.SendMessageBatchRequest(QueueUrl, sendMesssageReqs);
+            var result = _client.SendMessageBatchAsync(batchReq).ConfigureAwait(false).GetAwaiter().GetResult();
+
+
+            //Check result for failed messages
+            if (result.Failed.Count > 0)
+            {
+                var m = result.Failed.Count.ToString() + " messages to Amazon SQS failed. See internal NLog log for details.";
+
+                InternalLogger.Error(m);
+
+                foreach (var f in result.Failed)
+                {
+                    InternalLogger.Error("Message failed to send. " + f.Message + ". Code = " + f.Code + ", Id = " + f.Id + ", SenderFault = " + f.SenderFault.ToString());
+                }
+
+                if (ThrowExceptions) throw new Exception(m);
+            }
         }
 
         protected override void Dispose(bool disposing)
