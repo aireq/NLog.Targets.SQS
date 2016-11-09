d53d5d661876eda4e01c22acb8f3c059f89d65b9
Eric Anastas <aireq303@gmail.com>
2016-11-09 14:38:37 -0800

    Added nuspec file

--- NLog.Targets.SQS/Properties/AssemblyInfo.cs
+++ NLog.Targets.SQS/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.Target.SQS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NLog target for Amazon SQS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.Target.SQS")]
