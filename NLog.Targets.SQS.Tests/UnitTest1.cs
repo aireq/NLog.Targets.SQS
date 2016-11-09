b827a0aa7dd403768a65c1b51546afc690735f64
Eric Anastas <aireq303@gmail.com>
2016-11-08 19:31:43 -0800

    Added unit test

---
+++ NLog.Targets.SQS.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NLog.Targets.SQS.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                var logger = NLog.LogManager.GetCurrentClassLogger();
+                logger.Info("This message from NLog.Targets.SQS");
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
