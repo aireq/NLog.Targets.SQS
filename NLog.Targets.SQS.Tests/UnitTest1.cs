cbeef5ba6ba9d0529f5d94cb1f439bfc7547a048
Eric Anastas <aireq303@gmail.com>
2016-11-08 19:59:11 -0800

    Added code to assert contents of message read back from the test queue

--- NLog.Targets.SQS.Tests/UnitTest1.cs
+++ NLog.Targets.SQS.Tests/UnitTest1.cs
@@ -15,12 +15,30 @@ namespace NLog.Targets.SQS.Tests
         {
             try
             {
-                var logger = NLog.LogManager.GetCurrentClassLogger();
-                logger.Info("This message from NLog.Targets.SQS");
+                string testMessageBody = "This message from NLog.Targets.SQS";
+
+                NLog.Targets.SQS.AwsSqsTarget target = (NLog.Targets.SQS.AwsSqsTarget)NLog.LogManager.Configuration.FindTargetByName("SQS Target");
+
+                var region = Amazon.RegionEndpoint.GetBySystemName(target.RegionEndPoint);
+                using (var sqs_client = new Amazon.SQS.AmazonSQSClient(target.AwsAccessKeyId, target.AwsSecretAccessKey, region))
+                {
+                    var logger = NLog.LogManager.GetCurrentClassLogger();
+                    logger.Info(testMessageBody);
+
+                    System.Threading.Thread.Sleep(1000);
+
+                    var message = sqs_client.ReceiveMessage(target.QueueUrl);
+
+                    Assert.AreEqual(System.Net.HttpStatusCode.OK, message.HttpStatusCode);
+                    Assert.AreEqual(1, message.Messages.Count);
+                    Assert.AreEqual(testMessageBody, message.Messages[0].Body);
+
+                    sqs_client.DeleteMessage(target.QueueUrl, message.Messages[0].ReceiptHandle);
+                }
             }
             catch (Exception)
             {
-                
+
                 throw;
             }
         }
